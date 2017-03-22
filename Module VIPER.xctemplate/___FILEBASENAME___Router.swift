de8db89ceeeaf50b9974cf6266de8e080259f2e3
Brandon Leeds <brandonleeds@gmail.com>
2017-03-22 14:24:09 -0400

    Add method to create module in router

--- Module VIPER.xctemplate/___FILEBASENAME___Router.swift
+++ Module VIPER.xctemplate/___FILEBASENAME___Router.swift
@@ -11,5 +11,20 @@
 import UIKit
 
 class ___FILEBASENAMEASIDENTIFIER___Router: ___FILEBASENAMEASIDENTIFIER___WireframeProtocol {
-
+    
+    weak var viewController: UIViewController?
+    
+    static func createModule() -> UIViewController {
+        // Change to get view from storyboard if not using progammatic UI
+        let view = ___FILEBASENAMEASIDENTIFIER___ViewController()
+        let interactor = ___FILEBASENAMEASIDENTIFIER___Interactor()
+        let router = ___FILEBASENAMEASIDENTIFIER___Router()
+        let presenter = ___FILEBASENAMEASIDENTIFIER___Presenter(interface: view, interactor: interactor, router: router)
+        
+        view.presenter = presenter
+        interactor.presenter = presenter
+        router.viewController = view
+        
+        return view
+    }
 }
