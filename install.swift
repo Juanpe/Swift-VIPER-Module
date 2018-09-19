2982dd489a546998a1ecc9c407eb5c7c31374eea
siengnonta <siengnontapat@gmail.com>
2018-09-19 11:59:38 +0700

    Fix string count

--- install.swift
+++ install.swift
@@ -52,7 +52,7 @@ func shell(launchPath: String, arguments: [String]) -> String
     
     let data = pipe.fileHandleForReading.readDataToEndOfFile()
     let output = String(data: data, encoding: String.Encoding.utf8)!
-    if output.characters.count > 0 {
+    if output.count > 0 {
         //remove newline character.
         let lastIndex = output.index(before: output.endIndex)
         return String(output[output.startIndex ..< lastIndex])
